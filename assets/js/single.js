bfca4891cf8658ae042cf040d9f9451672b9cc01
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-14 02:50:39 -0700

    reshaped html

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,23 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=") [1];
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     //format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,12 +33,10 @@ var getRepoIssues = function(repo) {
                 if (response.headers.get("link")) {
                     displayWarning(repo);
                 }    
-                
             });
-        }
-        else {
-            console.log(response);
-            alert("There was a problem with your request!");
+        }else {
+            //if not successful, redirect to homepage
+            document.location.replace("index.html")
         }
     });
 };
@@ -32,12 +47,13 @@ var displayIssues = function(issues) {
         return;
     }
 
+    //loop over given issues
     for (var i = 0; i<issues.length; i++) {
         //create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blanck");
+        issueEl.setAttribute("target", "_blank");
 
         //create span to hold issue title
         var titleEl = document.createElement("span");
@@ -62,16 +78,15 @@ var displayIssues = function(issues) {
         //append to the dom
         issueContainerEl.appendChild(issueEl);
     }
-
 };    
     
 var displayWarning = function(repo) {
     //add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit";
 
-
+    //create link element
     var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.textContent = "Github.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
 
@@ -79,4 +94,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
